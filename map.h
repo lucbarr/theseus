d60ed903fd27fc4118425e6374b4b3b21c839664
Luciano Barreira <meckmao@gmail.com>
2016-04-06 18:04:36 -0300

    Map is now a class

---
+++ map.h
@@ -0,0 +1,44 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <string>
+#include <iostream>
+
+#include "util.h"
+
+const int MAX_SIZE = 10;
+
+class Map{
+private:
+	Pos startpos_;
+	Pos endpos_;
+	int n_of_rows_;
+public:
+	std::string	map_[MAX_SIZE];
+	Map(){};
+
+	void setStartPos (Pos startpos) {
+	 startpos_.x = startpos.x;
+	 startpos_.y = startpos.y;
+	}
+	void setEndPos (Pos endpos) { endpos_ = endpos; }
+	void setNofRows (int index) { n_of_rows_ = index; }
+
+	bool isEnd (Pos position) { return ((position.x == endpos_.x) && (position.y == endpos_.y)); }
+	bool isStart (Pos position) { return ((position.x == startpos_.x) && (position.y == startpos_.y)); }
+
+	int getNofRows() { return n_of_rows_; }
+	Pos getStartPos() { return startpos_; }
+	Pos getEndPos() { return endpos_; }
+	
+	void printMap();
+};
+
+
+void Map::printMap(){
+	for (int j=0 ; j < n_of_rows_ ; ++j){
+		std::cout << map_[j] << std::endl;
+	}
+}
+
+#endif 
